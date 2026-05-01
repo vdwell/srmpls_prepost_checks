2934f93085f4f26601365482d0fb94cfb9d1dbd5
Vladimir Deviatkin <vladimir.deviatkin@ericsson.com>
2026-05-01 12:24:20 +0200

    update1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-
-
+from .check
 
 
 def main():
@@ -14,16 +13,17 @@ def main():
     import json
     import logging
     from logging.handlers import RotatingFileHandler
-    from .utils import get_input_parameters
-
+   
 
     current_dir = Path(__file__).parent
     parent_dir = current_dir.parent
-    sys.path.append(str(parent_dir))
-    from utils import csv2list, yaml2dict, get_filnames_in_folder, text2list
+    superparent_dir = parent_dir.parent
+    sys.path.append(str(superparent_dir))
+    from utils import csv2list, yaml2dict, get_filnames_in_folder, text2list, get_input_parameters
+    from mp import connect_to_devices, disconnect_from_devices
 
     modules = ['main', 'connect']
-    log_directory: Path = parent_dir / 'logs'
+    log_directory: Path = superparent_dir / 'logs'
 
     root = logging.getLogger()
     root.setLevel(logging.INFO)
@@ -45,11 +45,11 @@ def main():
 
 
 
-    from .connect import connect_to_devices, disconnect_from_devices
+    
     # from .checks import check1, srmpls_ping, mpls_ping, srmpls_trace, mpls_trace, trace_result_analysis, ping_result_analysis
-    from .checks_reachability import check_reachability
-    from .checks_pre_srmpls import checks_pre_srmpls
-
+    # from .checks_reachability import check_reachability
+    # from .checks_pre_srmpls import checks_pre_srmpls
+    from .checks_ph
 
 
 
@@ -57,26 +57,27 @@ def main():
 
     logger.info("Main started")
 
-    mydict: dict[str,dict[str,str]] = get_input_parameters()
+    settings: dict[str,dict[str,str]] = get_input_parameters()
     print('-' * 50)
-    pprint.pprint(mydict)
+    pprint.pprint(settings)
     print('-' * 50)
 
-    routers_csv_file: Path = parent_dir / 'input' / mydict['parameters']['devices']
+    routers_csv_file: Path = superparent_dir / 'input' / settings['parameters']['devices']
     routers_details: list[list[str]] = csv2list(routers_csv_file)
 
-    hosts2ping: list[str] = text2list(parent_dir / 'input' / mydict['parameters']['hosts2ping'])
+    hosts2ping: list[str] = text2list(superparent_dir / 'input' / settings['parameters']['hosts2ping'])
     pprint.pprint(routers_details)
     pprint.pprint(hosts2ping)
     print('-' * 50)
 
-    connections:list[list[BaseConnection, str, str, str]] = connect_to_devices(mydict, routers_details)
+    connections:list[list[BaseConnection, str, str, str]] = connect_to_devices(settings, routers_details)
 
 
 
-
+    
     # prechecks before srmpls configuration
-    checks_pre_srmpls(connections, mydict['parameters']['isis_process_name'], mydict['parameters']['sr_blocks'])
+    if settings['checks']['phase0_pre']: # phase0_pre is True
+        checks_pre_srmpls(connections, settings['parameters']['isis_process_name'], settings['parameters']['sr_blocks'])
 
     # check mpls reachability
     # check_reachability(connections, hosts2ping)
@@ -97,8 +98,6 @@ def main():
 
 
     logger.info("Main finished")
-
-
 
 if __name__ == "__main__":
     main()
